46c01741817ffe49357c19e0558da9eb2a41c80a
UVM10 <mn4546615@gmail.com>
2025-02-22 03:24:30 +0000

    Du an moi

---
+++ Matrix.c
@@ -0,0 +1,13 @@
+/*Matrix Calculation Program
+Input: size, numbers, Operation
+Output: matrixes, solution for equation
+Features: +, -, ., T, Equation
+Deadline: 08/02/2025*/
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<ctype.h>
+int main(int argc, char* argv[])
+{
+return 0;
+}
